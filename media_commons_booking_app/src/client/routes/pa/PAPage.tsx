294f67852bf472bb8b40258a7b4968a55ae0f9d0
lucia <51058748+lucia-gomez@users.noreply.github.com>
2024-06-09 20:32:32 -0400

    remove pa page tabs

--- media_commons_booking_app/src/client/routes/pa/PAPage.tsx
+++ media_commons_booking_app/src/client/routes/pa/PAPage.tsx
@@ -31,35 +31,7 @@ const PAPage = () => {
         </div>
       ) : (
         <div>
-          <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
-            <li className="mr-2">
-              <a
-                onClick={() => setTab('bookings')}
-                aria-current="page"
-                className={`${
-                  tab === 'bookings'
-                    ? 'inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500'
-                    : 'inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300 '
-                }`}
-              >
-                Bookings
-              </a>
-            </li>
-            <li className="mr-2">
-              <a
-                onClick={() => setTab('safety_training')}
-                className={`${
-                  tab === 'safety_training'
-                    ? 'inline-block active dark:bg-gray-800 dark:text-blue-500'
-                    : 'inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300 '
-                }`}
-              >
-                Safety Training
-              </a>
-            </li>
-          </ul>
-          {tab === 'safety_training' && <SafetyTrainedUsers />}
-          {tab === 'bookings' && <Bookings isPaView={true} />}
+          <Bookings isPaView={true} />
         </div>
       )}
     </div>
